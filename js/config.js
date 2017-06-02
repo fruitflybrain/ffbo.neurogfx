6fd4e164842052cdeb90861c6741a0bbf1403cdd
Yiyin Zhou <yiyin@ee.columbia.edu>
2017-06-02 15:55:18 -0400

    changed js file links to https

--- js/config.js
+++ js/config.js
@@ -16,11 +16,11 @@ var require = {
     mmenu: 'https://cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/5.7.8/js/jquery.mmenu.all.min.js',
     three: 'https://cdnjs.cloudflare.com/ajax/libs/three.js/78/three',
     TrackballControls: 'js/TrackballControls',
-    Lut: "http://cdn.rawgit.com/mrdoob/three.js/dev/examples/js/math/Lut",
-    FFBOMesh3D: "http://cdn.rawgit.com/fruitflybrain/ffbo.visualizer_module/master/mesh3d",
-    resizesensor: "http://cdn.rawgit.com/marcj/css-element-queries/master/src/ResizeSensor",
+    Lut: "https://cdn.rawgit.com/mrdoob/three.js/dev/examples/js/math/Lut",
+    FFBOMesh3D: "https://cdn.rawgit.com/fruitflybrain/ffbo.visualizer_module/master/mesh3d",
+    resizesensor: "https://cdn.rawgit.com/marcj/css-element-queries/master/src/ResizeSensor",
 
-    detector: 'http://cdn.rawgit.com/mrdoob/three.js/dev/examples/js/Detector',
+    detector: 'https://cdn.rawgit.com/mrdoob/three.js/dev/examples/js/Detector',
     d3: 'https://d3js.org/d3.v3.min',
     graphics_explorer: 'js/graphics_explorer',
   }
